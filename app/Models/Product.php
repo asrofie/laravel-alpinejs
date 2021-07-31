07da8053c46a752c545de13d1579319ccd1fe3a6
Asrofie <asrofie@gmail.com>
2021-08-01 02:01:23 +0700

    adding column like_count

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,5 +9,5 @@ class Product extends Model
 {
     use HasFactory;
     protected $table='product';
-    protected $fillable=['name','image','price'];
+    protected $fillable=['name','image','price', 'like_count'];
 }
