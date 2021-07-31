e1e09069a0b00df550fcf86a68bfe5a534ad21a7
Asrofie <asrofie@gmail.com>
2021-07-31 18:32:53 +0700

    Data setup

---
+++ database/migrations/2021_07_31_110753_create_cart_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('user_id', false);
+            $table->integer('product_id', false);
+            $table->integer('qty', false);
+            $table->float('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = new User([
+            'name' => 'Customer 1',
+            'email'=> 'customer1@domain.com',
+            'password' => bcrypt('12345')
+        ]);
+        $user->save();
+        $p1 = $this->p('Sepatu Adidas', 'item-1.png', '549000');
+        $p1->save();
+        $p2 = $this->p('Sepatu Pro-ATT', 'item-2.png', '870500');
+        $p2->save();
+        $p3 = $this->p('Sepatu Nike', 'item-3.png', '349000');
+        $p3->save();
+        $this->c($user->id, $p1->id, random_int(1,10), $p1->price);
+        $this->c($user->id, $p2->id, random_int(1,10), $p2->price);
+        $this->c($user->id, $p3->id, random_int(1,10), $p3->price);
     }
+
+    private function c($user, $product, $qty, $price) {
+        $cart = new Cart([
+            'user_id'=> $user,
+            'product_id'=> $product,
+            'qty' => $qty,
+            'price' => $price
+        ]);
+        $cart->save();
+        return $cart;
+    }
+
+    private function p($name, $image, $price) {
+        return new Product([
+            'name' => $name,
+            'image' =>$image,
+            'price'=>$price
+        ]);
+    }
+
 }
