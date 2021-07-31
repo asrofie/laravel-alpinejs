3fa5d1004aa9870f09b90e21f3ee671648247a68
Asrofie <asrofie@gmail.com>
2021-08-01 02:01:36 +0700

    adding api update qty

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/cart', function(Request $request) {
     return CartResource::collection(Cart::all());
 });
+Route::get('/cart/{id}/save', function(Request $request, $id) {
+    $qty = $request->get('qty');
+    $cart = Cart::find($id);
+    if (!$cart) {
+        return response(['data' => 'id not found', 'status' => false]);
+    }
+    $qty = intval($qty);
+    if ($qty == 0) {
+        return response(['data' => 'qty should be greater than 0', 'status' => false]);
+    }
+    $cart->qty = $qty;
+    $cart->save();
+    return response(['data' => null, 'status' => true]);
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
